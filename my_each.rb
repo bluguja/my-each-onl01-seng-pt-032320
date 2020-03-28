de4e1f3df2e3fa1855d7e8c47bfb6104474f393e
Bala Luguja <bluguja@gmail.com>
2020-03-28 18:09:07 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,16 @@
-def my_each # put argument(s) here
-  # code here
-end
+
+def my_each(array)
+  array=[1,2,3,4]
+  
+    i = 0
+    
+    while i < array.length
+      yield(array[i])
+ 
+    #puts "array is #{yeild}"
+  
+    i+= 1
+    end
+  end
+
+
